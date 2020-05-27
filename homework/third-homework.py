ea55710954c482487e7f76a7be9231d55942b644
Lee-Saeun <59189140+Lee-Saeun@users.noreply.github.com>
2020-05-27 22:33:36 +0900

    3주차 수업

---
+++ homework/third-homework.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# select를 이용해서, tr들을 불러오기
+# movies = soup.select('.box_type_1.mb_8 > ul > li >a')
+# print(len(movies))
+# print(movies[0])
+# print(movies[0].text)
+
+import requests
+from bs4 import BeautifulSoup
+
+# URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg=1',headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# select를 이용해서, tr들을 불러오기
+
+musics = soup.select('div.music-list-wrap > table.list-wrap > tbody > tr')
+for music in musics :
+    rank = music.select_one('td.number').text.split("\n")[0]
+#print(rank)
+    title = music.select_one('td.info > a').text.strip()
+#print(title)
+    artist = music.select_one('td.info > a.artist').text.strip()
+#print(artist)
+    print(rank, title, artist)
+
+
--- memo/first-html/third_python/02_hello.py
+++ memo/first-html/third_python/02_hello.py
@@ -1,15 +1,11 @@
 import requests # requests 라이브러리 설치 필요
 
 r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
-rjson = r.json()
-print(rjson['RealtimeCityAir']['row'][0]['NO2'])
-
-import requests # requests 라이브러리 설치 필요
-
-r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
-rjson = r.json()
-
-gus = rjson['RealtimeCityAir']['row']
-
-for gu in gus:
-	print(gu['MSRSTE_NM'], gu['IDEX_MVL'])
+rjson = r.json() #json형식으로 데이터를 변환해서 rjson이라는 변수에 담아준다! 
+rows = rjson['RealtimeCityAir']['row']
+for i in rows :
+    gu = i['MSRSTE_NM']
+    mise = i['IDEX_MVL']
+    #print(gu , mise)
+    if mise > 50 :
+        print('미세먼지 위험 구:'+ gu, mise)
