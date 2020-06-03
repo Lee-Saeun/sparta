9cfd8a5a2f9d1af6d8d34b2fed604d1e39fbf95a
Lee-Saeun <59189140+Lee-Saeun@users.noreply.github.com>
2020-06-04 00:53:03 +0900

    4주차 후반부 수업

--- projects/prac/app.py
+++ projects/prac/app.py
@@ -9,15 +9,15 @@ def home():
 ## API 역할을 하는 부분
 @app.route('/test', methods=['POST']) #method다르면 경로 겹쳐도 됨! 
 def test_post():
-   title_receive = request.form['title_give'] 
+   title_receive = request.form['title_give'] #form을 통해 
    print(title_receive)
-   return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+   return jsonify({'result':'success', 'msg': '이 요청은 POST!','title':title_receive})
 
 @app.route('/test', methods=['GET'])
 def test_get():
    title_receive = request.args.get('title_give') # args에 쿼리 파라미터를 저장 , title_receive에 저장한다 . 데이터를 가져오려면 args.get('키값')
    print(title_receive) #요청한거 다 기록됨 
-   return jsonify({'result':'success', 'msg': '이 요청은 GET!'}) 
+   return jsonify({'result':'success', 'msg': '이 요청은 GET!','title':title_receive}) #응답값이 항상 고정되어 있음 
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
